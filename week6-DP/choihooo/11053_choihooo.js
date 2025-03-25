6ddb4b1000b68587f753319c1a8f664636cdc0e4
choihooo <hochoi8621@naver.com>
2025-03-25 18:30:36 +0900

    Create 11053_choihooo

---
+++ week6-DP/choihooo/11053_choihooo.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const test_num = Number(input.shift());
+const test_case = input.shift().split(" ").map(Number);
+
+const dp = new Array(test_num);
+dp[0] = 1;
+
+for (let i = 1; i < test_num; i++) {
+  let max = 0;
+  for (let j = 0; j < i; j++) {
+    if (test_case[j] < test_case[i]) {
+      max = Math.max(max, dp[j]);
+    }
+  }
+  dp[i] = max + 1;
+}
+
+console.log(Math.max(...dp));
