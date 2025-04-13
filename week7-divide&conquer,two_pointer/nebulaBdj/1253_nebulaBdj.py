87a327f085ea2622fead0199063bcd120454dfa9
nebulaBdj <dongdung5070@gmail.com>
2025-04-13 22:19:30 +0900

    Create 1253_nebulaBdj

---
+++ week7-divide&conquer,two_pointer/nebulaBdj/1253_nebulaBdj.py
@@ -0,0 +1,19 @@
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = list(map(int, sys.stdin.readline().split()))
+    arr.sort()
+    ans = 0
+
+    for i in range(N):
+        tmp = arr[:i] + arr[i + 1:]
+        left, right = 0, len(tmp) - 1
+        while left < right:
+            t = tmp[left] + tmp[right]
+            if t == arr[i]:
+                ans += 1
+                break
+            if t < arr[i]: left += 1
+            else: right -= 1
+    print(ans)
